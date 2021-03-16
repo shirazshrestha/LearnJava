8bad3cbe4e077277ba022bd00a9c75483c833f08
Santosh Lal Karna <santosh.karna@dryicesolutions.net>
2021-03-16 07:24:31 +0545

    completed Random class code

--- src/classworks/Random.java
+++ src/classworks/Random.java
@@ -1,9 +1,11 @@
 package classworks;
 
+//This is java class
+//this class generates random number
 public class Random {
 
     public static void main(String[] args) {
-        System.out.println(Math.random());
+        System.out.println((int)(Math.random()*100));
     }
 
 }
